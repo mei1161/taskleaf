e10afbb42f95e8c47189fe6b759864ba86950ac1
mei1161 <ichigo_happy@ezweb.ne.jp>
2020-04-24 22:28:18 +0900

    Set to not include comma in title

--- db/migrate/20200424070029_change_task_name_limit30.rb
+++ db/migrate/20200424070029_change_task_name_limit30.rb
@@ -1,4 +1,11 @@
 class ChangeTaskNameLimit30 < ActiveRecord::Migration[6.0]
-  def change
+  def up
+    change_to_column :tasks, :name, :string, limit: 30
   end
+
+  def down
+    change_column :tasks, :name, :string
+  end
+
+
 end
