79ad900bc17b880d779d638fbfc8a3d37a2d6225
mei1161 <ichigo_happy@ezweb.ne.jp>
2020-04-24 15:52:37 +0900

    Add not null constraint to name column

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_23_044023) do
+ActiveRecord::Schema.define(version: 2020_04_24_064817) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
