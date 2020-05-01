a7f1764030c3a46a0092812eacddb6bb5c807d17
mei1161 <ichigo_happy@ezweb.ne.jp>
2020-05-01 13:30:05 +0900

    Associate users with tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_24_145311) do
+ActiveRecord::Schema.define(version: 2020_05_01_010410) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
