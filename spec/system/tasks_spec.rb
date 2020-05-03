bb609a6f3229b67b27fc5a401a206945e3141052
mei1161 <ichigo_happy@ezweb.ne.jp>
2020-05-03 18:29:18 +0900

    Create user B, log in, and test if the tasks edited by other users are visible.

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -22,5 +22,23 @@ describe 'タスク管理機能', type: :system do
         expect(page).to have_content '最初のタスク'
       end
     end
+
+    context 'ユーザーBがログインしているとき' do
+      before do
+        # ユーザーBを作成しておく
+        FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com')
+        # ユーザーBでログインする
+        visit login_path
+        fill_in 'メールアドレス', with: 'b@example.com'
+        fill_in 'パスワード', with: 'password'
+        click_button 'ログインする'
+      end
+
+      it 'ユーザーAが作成したタスクが表示されない' do
+        # ユーザーAが作成したタスクの名称が画面上に表示されていないことを確認
+        expect(page).to have_no_content '最初のタスク'
+      end
+
+    end
   end
 end
