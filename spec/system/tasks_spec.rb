1d4d33ca4715f08e34d104488e4d247820e86c4c
mei1161 <ichigo_happy@ezweb.ne.jp>
2020-05-04 16:59:30 +0900

    Sharing the same test contents

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -13,14 +13,15 @@ describe 'タスク管理機能', type: :system do
       click_button 'ログインする'
     end
 
+    shared_examples_for 'ユーザーAが作成したタスクが表示される' do
+      it { expect(page).to have_content '最初のタスク'}
+    end
+
   describe '一覧表示機能' do
     context 'ユーザーAがログインしているとき' do
       let(:login_user) { user_a }
 
-      it 'ユーザーAが作成したタスクが表示される' do
-        # 作成済みのタスクの名称が画面上に表示されていることを確認
-        expect(page).to have_content '最初のタスク'
-      end
+      it_behaves_like 'ユーザーAが作成したタスクが表示される'
     end
 
     context 'ユーザーBがログインしているとき' do
@@ -41,9 +42,7 @@ describe 'タスク管理機能', type: :system do
         visit task_path(task_a)
       end
 
-      it 'ユーザーAが作成したタスクが表示される' do
-        expect(page).to have_content '最初のタスク'
-      end
+      it_behaves_like 'ユーザーAが作成したタスクが表示される'
     end
   end
 
