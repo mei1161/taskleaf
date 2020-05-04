3e739576d3539727202e571d7d1d6257627c8dc7
mei1161 <ichigo_happy@ezweb.ne.jp>
2020-05-05 00:53:56 +0900

    fix error

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -26,13 +26,13 @@ class TasksController < ApplicationController
   end
 
   def update
-    task.update!(task_params)
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
+    @task.update!(task_params)
+    redirect_to tasks_url, notice: "タスク「#{@task.name}」を更新しました。"
   end
 
   def destroy
-    task.destroy
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
+    @task.destroy
+    redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました。"
   end
 
   def confirm_new
