ee59b6cd1482118f33b30c02188b26bf705193a5
mei1161 <ichigo_happy@ezweb.ne.jp>
2020-05-05 23:12:31 +0900

    Limit the range of columns that can be used for search

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,12 @@
 class Task < ApplicationRecord
+  def self.ransackable_attributes(auth_object = nil)
+    %w[name created_at]
+  end
+
+  def self.ransackable_association(auth_object = nil)
+    []
+  end
+
   validates :name, presence: true
   validates :name, length: { maximum: 30 }
   validate :validate_name_not_including_comma
