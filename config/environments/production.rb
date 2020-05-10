bf15e726987a69ba46b90c06ccd773c742e0eb57
mei1161 <ichigo_happy@ezweb.ne.jp>
2020-05-10 20:55:37 +0900

    fix code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -116,7 +116,7 @@ Rails.application.configure do
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
     domain: ENV['MAIL_DOMAIN'],
-    address: 'smtp.dendgrid.net',
+    address: 'smtp.sendgrid.net',
     port: 2525,
     authentication: :plain,
     enable_starttls_auto: true
